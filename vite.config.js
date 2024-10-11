9a7d107a9e1e8ffe6a326cb97d337ee597a8d193
Gustavo Queiroz <compgustavo@edu.unifil.br>
2024-10-11 20:39:39 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: "Eldritch Visions",
+        short_name: "Eldritch AR",
+        description: "A simple AR app for visualizing D&D stuff",
+        theme_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/eldritch_visions/'
+      },
+      workbox: {
+        cleanupOutdatedCaches: false,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
+      },
+      registerType: 'autoUpdate',
+      injectRegister: 'auto'
+    })
+  ],
+  define: {
+    'process.env': {}
+  },
+  base: '/eldritch_visions/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
